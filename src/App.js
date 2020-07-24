68731c7e2b83455f097776e3badc9586d19cdbc1
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-24 11:54:31 -0400

    Complete home page, such as it is

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import {Route, Router} from 'react-router-dom';
+import {Route, Router, Link} from 'react-router-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import { Paper } from '@material-ui/core';
+import { Button, FormControl, Paper } from '@material-ui/core';
 import './App.css';
 import 'fontsource-roboto';
 
@@ -19,6 +19,9 @@ export default function App () {
         <Route exact path="/">
           <Paper style={{height: "100vh"}}>
             <Topbar/>
+            <Link to="/pizza">
+              <Button variant="contained">Order a Pizza!</Button>
+            </Link>
           </Paper>
         </Route>
         <Route path="/pizza">
